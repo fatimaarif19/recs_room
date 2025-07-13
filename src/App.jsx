8b923ce5229eb812018bb42e6c89d0f17d10a645
fatimaarif19 <fatima.arif13071989@gmail.com>
2025-07-13 13:23:56 +0530

    tailwind added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-pink-600">
+        Tailwind is working! 💖
+      </h1>
+      <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        Test Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
